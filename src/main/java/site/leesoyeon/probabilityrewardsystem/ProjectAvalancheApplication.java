30d4ca1210b245e3ac97efe66feb30457f0ac4ac
ssomal62 <wxy890@gmail.com>
2024-08-09 01:53:25 +0900

    🎉 init: 스프링 프로젝트 초기 세팅

---
+++ src/main/java/site/leesoyeon/probabilityrewardsystem/ProjectAvalancheApplication.java
@@ -0,0 +1,13 @@
+package site.leesoyeon.probabilityrewardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAvalancheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAvalancheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/leesoyeon/probabilityrewardsystem/ProjectAvalancheApplicationTests.java
@@ -0,0 +1,13 @@
+package site.leesoyeon.probabilityrewardsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAvalancheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
