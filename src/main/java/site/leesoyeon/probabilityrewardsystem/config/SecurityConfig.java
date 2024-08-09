428588da98734fd966611aad928aa5837a907e33
ssomal62 <wxy890@gmail.com>
2024-08-09 20:06:19 +0900

    ✨ feat: SecurityConfig 및 SwaggerConfig 구현
    - CORS 설정 및 인터셉터 등록을 위한 SecurityConfig 구현
    - Swagger를 통한 API 문서화를 위한 SwaggerConfig 설정 추가

---
+++ src/main/java/site/leesoyeon/probabilityrewardsystem/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package site.leesoyeon.probabilityrewardsystem.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**"
+                ).permitAll()
+                .anyRequest().authenticated())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://leesoyeon.site");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
