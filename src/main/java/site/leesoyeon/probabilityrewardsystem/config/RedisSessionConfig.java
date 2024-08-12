d33b222d1c1721e5a5385c8834a3ff8bb2ac5004
ssomal62 <wxy890@gmail.com>
2024-08-12 16:08:05 +0900

    ✨ feat: 이메일 인증 토큰 만료 시간 설정을 위한 Bean 추가
    - 이메일 인증 토큰 만료 시간(emailVerificationTokenExpiration)을 30분으로 설정하는 Bean 추가

--- src/main/java/site/leesoyeon/probabilityrewardsystem/config/RedisSessionConfig.java
+++ src/main/java/site/leesoyeon/probabilityrewardsystem/config/RedisSessionConfig.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -42,7 +41,12 @@ public class RedisSessionConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public long emailVerificationTokenExpiration() {
+        return 1800L; // 30분 (초 단위)
     }
 }
