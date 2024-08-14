475fde25b37bc863d49785283c5560c6f5296fdb
ssomal62 <wxy890@gmail.com>
2024-08-14 23:57:43 +0900

    🚚 feat(shipping): 배송 관리 기능 구현
    - Shipping 엔티티 및 관련 클래스 추가
      - 배송 상태 관리 및 수취인 정보 업데이트 메서드 구현
    - ShippingService 구현
      - 배송 상태 업데이트 및 주문 상태와의 연동 로직 추가
    - ShippingRepository 추가
      - Shipping 엔티티를 위한 JPA 리포지토리 인터페이스 생성
    - ShippingMapper 추가
      - ShippingInfo DTO를 Shipping 엔티티로 변환하는 매핑 로직 구현
    - 예외 처리 클래스 추가
      - ShippingException, ShippingInfoInvalidException 클래스 생성
    - ShippingStatus Enum 추가
      - 배송 상태를 관리하기 위한 열거형 정의

---
+++ src/main/java/site/leesoyeon/probabilityrewardsystem/shipping/service/ShippingService.java
@@ -0,0 +1,50 @@
+package site.leesoyeon.probabilityrewardsystem.shipping.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.leesoyeon.probabilityrewardsystem.order.entity.Order;
+import site.leesoyeon.probabilityrewardsystem.order.exception.OrderException;
+import site.leesoyeon.probabilityrewardsystem.order.repository.OrderRepository;
+import site.leesoyeon.probabilityrewardsystem.shipping.dto.ShippingStatusDto;
+import site.leesoyeon.probabilityrewardsystem.shipping.entity.Shipping;
+import site.leesoyeon.probabilityrewardsystem.shipping.exception.ShippingException;
+import site.leesoyeon.probabilityrewardsystem.shipping.repository.ShippingRepository;
+import site.leesoyeon.probabilityrewardsystem.shipping.util.ShippingMapper;
+
+import java.util.UUID;
+
+import static site.leesoyeon.probabilityrewardsystem.common.enums.ApiStatus.NOT_FOUND_ORDER;
+import static site.leesoyeon.probabilityrewardsystem.common.enums.ApiStatus.NOT_FOUND_SHIPPING_ADDRESS;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ShippingService {
+
+    private final ShippingRepository shippingRepository;
+    private final OrderRepository orderRepository;
+    private final ShippingMapper shippingMapper;
+
+    @Transactional
+    public void updateShippingStatus(ShippingStatusDto request) {
+        Shipping shipping = findShippingById(request.shippingId());
+        shipping.updateStatus(request.status());
+
+        Order order = orderRepository.findByShippingId(request.shippingId())
+                .orElseThrow(() -> new OrderException(NOT_FOUND_ORDER));
+
+        order.updateStatusBasedOnShipping(request.status());
+        orderRepository.save(order);
+    }
+
+//     ============================================
+//                  Private Methods
+//     ============================================
+
+    private Shipping findShippingById(UUID shippingId) {
+        return shippingRepository.findById(shippingId).orElseThrow(() -> new ShippingException(NOT_FOUND_SHIPPING_ADDRESS));
+    }
+}
