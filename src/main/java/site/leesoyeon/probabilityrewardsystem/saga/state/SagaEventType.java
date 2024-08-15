aa9c0303cc78f8107135bf6880d18e420e785264
ssomal62 <wxy890@gmail.com>
2024-08-15 21:29:26 +0900

    🌀 feat(saga): 사가 상태 및 이벤트 타입 확장
    - `SagaEventType`에 `ORDER_COMPLETED` 및 `SHIPPING_CREATED` 이벤트 타입을 추가하여 사가 단계의 다양한 이벤트를 처리할 수 있도록 확장.
    - `SagaState`에 `POINT_REFUNDED` 상태를 추가하여 포인트 환불 작업에 대한 상태 관리 강화.
    - 사가 상태 및 이벤트 타입의 확장으로 인해 다양한 시나리오에서 사가 패턴을 보다 유연하게 처리할 수 있도록 개선.

--- src/main/java/site/leesoyeon/probabilityrewardsystem/saga/state/SagaEventType.java
+++ src/main/java/site/leesoyeon/probabilityrewardsystem/saga/state/SagaEventType.java
@@ -3,7 +3,8 @@ package site.leesoyeon.probabilityrewardsystem.saga.state;
 public enum SagaEventType {
     POINT_DEDUCTION,
     INVENTORY_DEDUCTION,
-    ORDER_CREATION,
+    ORDER_COMPLETED,
+    SHIPPING_CREATED,
     SAGA_FAILED,
     COMPENSATION_STARTED,
     COMPENSATION_COMPLETED,
