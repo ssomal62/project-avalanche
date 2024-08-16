c98cd641ef6f13acebe667f9f364603db66bf98b
ssomal62 <wxy890@gmail.com>
2024-08-17 02:15:08 +0900

    ✨ feat(shipping): 'avalanche-shipping' 서비스 초기 세팅
    - 'avalanche-shipping' 모듈 생성.
    - Spring Boot 기본 의존성 및 배송 관리 관련 필수 의존성 추가.
    - 배송지 등록, 수정, 삭제, 조회 기능을 위한 기본 구조 설정.

---
+++ avalanche-shipping/src/main/java/site/leesoyeon/avalanche/shipping/AvalancheShippingApplication.java
@@ -0,0 +1,17 @@
+package site.leesoyeon.avalanche.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients(basePackages = "site.leesoyeon.avalanche.shipping.client")
+public class AvalancheShippingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvalancheShippingApplication.class, args);
+    }
+
+}
---
+++ avalanche-shipping/src/test/java/site/leesoyeon/avalanche/shipping/AvalancheShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.leesoyeon.avalanche.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalancheShippingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
