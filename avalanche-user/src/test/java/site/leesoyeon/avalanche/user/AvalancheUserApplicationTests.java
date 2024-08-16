55ca5d6215de24150f20180ed996699eba91bb23
ssomal62 <wxy890@gmail.com>
2024-08-17 02:11:39 +0900

    ✨ feat(user): 'avalanche-user' 서비스 초기 세팅
    - 'avalanche-user' 모듈 생성.
    - Spring Boot 기본 의존성 및 유저 서비스 관련 필수 의존성 추가.
    - 사용자 관리 및 인증을 위한 기본 구조 설정.

---
+++ avalanche-user/src/test/java/site/leesoyeon/avalanche/user/AvalancheUserApplicationTests.java
@@ -0,0 +1,13 @@
+package site.leesoyeon.avalanche.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalancheUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
