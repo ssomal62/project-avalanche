8417e3cafa4856d049fd671222c473fca996e6ef
ssomal62 <wxy890@gmail.com>
2024-08-27 10:24:47 +0900

    🔧 chore: OrderMapper에 `OrderDetailDto` 매핑 메서드 추가
    - `toOrderDetailDto(Order order)` 메서드 추가하여 `Order` 엔티티를 `OrderDetailDto`로 매핑

--- order-service/src/main/java/site/leesoyeon/avalanche/order/application/util/OrderMapper.java
+++ order-service/src/main/java/site/leesoyeon/avalanche/order/application/util/OrderMapper.java
@@ -4,10 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
-import site.leesoyeon.avalanche.order.presentation.dto.OrderDetailDto;
 import site.leesoyeon.avalanche.order.domain.model.Order;
+import site.leesoyeon.avalanche.order.presentation.dto.ShippingInfo;
+import site.leesoyeon.avalanche.order.presentation.dto.OrderDetailDto;
+import site.leesoyeon.avalanche.order.presentation.dto.OrderRequest;
 import site.leesoyeon.avalanche.order.shared.enums.OrderStatus;
-import site.leesoyeon.avalanche.order.saga.dto.OrderContext;
 
 
 @Mapper(componentModel = "spring", imports = {OrderStatus.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -16,10 +17,11 @@ public interface OrderMapper {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
     @Mapping(target = "orderId", ignore = true)
-    @Mapping(target = "shippingId", ignore = true)
     @Mapping(target = "status", constant = "CREATED")
-    @Mapping(source = "productInfo.productId", target = "productId")
-    Order toEntity(OrderContext context);
+    @Mapping(target = "productId", source = "productInfo.productId")
+    Order toEntity(OrderRequest dto);
 
     OrderDetailDto toOrderDetailDto(Order order);
+
+    ShippingInfo toShippingInfoDto(ShippingInfo shippingInfo);
 }
