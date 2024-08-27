13325afc93a8167b945af92aee3c4ffd6c50213d
ssomal62 <wxy890@gmail.com>
2024-08-27 11:17:57 +0900

    🔧 refactor: `AvalancheProductApplication` 설정 업데이트
    - `@EnableJpaAuditing` 추가: JPA 엔티티의 자동 감사를 활성화
    - `@EnableScheduling` 추가: 스케줄링 기능 활성화
    - `@SpringBootApplication` 및 `@EnableFeignClients` 기본 설정 유지

--- product-service/src/main/java/site/leesoyeon/avalanche/product/AvalancheProductApplication.java
+++ product-service/src/main/java/site/leesoyeon/avalanche/product/AvalancheProductApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
+@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 @EnableFeignClients(basePackages = "site.leesoyeon.avalanche.product.infrastructure.external.client")
 public class AvalancheProductApplication {
