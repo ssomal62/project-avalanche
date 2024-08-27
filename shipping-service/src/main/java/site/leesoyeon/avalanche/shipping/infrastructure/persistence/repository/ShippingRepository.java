b10a16a1e110c40224aac7733ed0d51fb0d54198
ssomal62 <wxy890@gmail.com>
2024-08-27 11:35:50 +0900

    ♻️ refactor: `ShippingRepository` 수정 및 `findByOrderId` 메서드 추가
    - `ShippingRepository`에 `findByOrderId` 메서드 추가:
      - `UUID orderId`를 기준으로 배송 정보를 조회하는 기능 추가.

--- shipping-service/src/main/java/site/leesoyeon/avalanche/shipping/infrastructure/persistence/repository/ShippingRepository.java
+++ shipping-service/src/main/java/site/leesoyeon/avalanche/shipping/infrastructure/persistence/repository/ShippingRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.leesoyeon.avalanche.shipping.domain.model.Shipping;
 
-
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ShippingRepository extends JpaRepository<Shipping, UUID> {
+    Optional<Shipping> findByOrderId(UUID orderId);
 }
