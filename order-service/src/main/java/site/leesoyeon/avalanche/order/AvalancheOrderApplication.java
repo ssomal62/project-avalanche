d1da86aeaa2d9f56a658503af34b4b9daf8c6147
ssomal62 <wxy890@gmail.com>
2024-08-27 10:09:06 +0900

    ♻️ refactor: AvalancheOrderApplication 클래스 리팩토링
    - @EnableFeignClients 애노테이션 제거 및 @EnableScheduling, @EnableJpaAuditing 추가
    - 애플리케이션에 스케줄링 및 JPA 감사 기능을 활성화

--- order-service/src/main/java/site/leesoyeon/avalanche/order/AvalancheOrderApplication.java
+++ order-service/src/main/java/site/leesoyeon/avalanche/order/AvalancheOrderApplication.java
@@ -3,11 +3,13 @@ package site.leesoyeon.avalanche.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients(basePackages = "site.leesoyeon.avalanche.order.infrastructure.external.client")
+@EnableScheduling
+@EnableJpaAuditing
 public class AvalancheOrderApplication {
 
     public static void main(String[] args) {
