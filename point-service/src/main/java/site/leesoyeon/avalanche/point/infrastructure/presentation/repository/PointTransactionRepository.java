eba5005fe2b6b1f89bbaa4bb8e300f6726212f15
ssomal62 <wxy890@gmail.com>
2024-08-27 11:05:43 +0900

    ✨ feat: PointTransactionRepository에 주문 ID 기반 조회 메서드 추가
    - `findByOrderIdAndIsCancelledFalse` 메서드 추가:
      - 취소되지 않은 특정 주문 ID에 대한 포인트 트랜잭션 조회 기능 추가

--- point-service/src/main/java/site/leesoyeon/avalanche/point/infrastructure/presentation/repository/PointTransactionRepository.java
+++ point-service/src/main/java/site/leesoyeon/avalanche/point/infrastructure/presentation/repository/PointTransactionRepository.java
@@ -3,10 +3,10 @@ package site.leesoyeon.avalanche.point.infrastructure.presentation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.leesoyeon.avalanche.point.domain.model.PointTransaction;
 
-
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PointTransactionRepository extends JpaRepository<PointTransaction, UUID>, PointTransactionRepositoryCustom {
 
-    PointTransaction findTopByUserIdOrderByCreatedDateDesc(UUID userId);
+    Optional<PointTransaction> findByOrderIdAndIsCancelledFalse(UUID orderId);
 }
